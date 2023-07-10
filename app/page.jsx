e37401ef9170d27e1e96502293dff28d02ce5d54
WebRizen <webrizen@gmail.com>
2023-07-10 11:56:56 +0530

    push

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import styles from '@/app/page.module.css';
 import Image from 'next/image';
 import heroImg from '@/app/assets/hero.png';
 import { BiSolidOffer } from 'react-icons/bi';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,7 +16,9 @@ export default function Home() {
           <p>
             Wear your favorite code with fashion. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
           </p>
+          <Link href='/shop'>
           <button>Explore</button>
+          </Link>
       </div>
       <div className={styles.right}>
         <Image src={heroImg} alt='Hero Image' />
